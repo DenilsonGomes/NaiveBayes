f85832d460dab5ffe18cce30f3eb48c130c5c779
Denilson Gomes <denilsongomesdg97@gmail.com>
2019-10-22 20:08:25 -0300

    Naive Bayes corrigido

--- naiveBayes.m
+++ naiveBayes.m
@@ -1,6 +1,6 @@
 %Autor: Denilson Gomes Vaz da Silva
 %Graduando em Engenharia da Computação
-%reconhecimento de Padroes
+%Reconhecimento de Padroes
 
 clear %limpa as variaveis
 clc %limpa o visor 
@@ -23,77 +23,43 @@ b3 = randn(1000,1) + 7; %atributo b
 a4 = randn(1000,1) + 7; %atributo a
 b4 = randn(1000,1) + 7; %atributo b
 
-%garantir mesmo desvio padrão para todos
+%garantir exatamente o mesmo desvio padrão para todos os atributos
 %--- Classe 1 -----
-a1 = a1/std(a1); %atributo a
-b1 = b1/std(b1); %atributo b
-
-%--- Classe 2 -----
-a2 = a2/std(a2); %atributo a
-b2 = b2/std(b2); %atributo b
-
-%--- Classe 3 -----
-a3 = a3/std(a3); %atributo a
-b3 = b3/std(b3); %atributo b
-
-%--- Classe 4 -----
-a4 = a4/std(a4); %atributo a
-b4 = b4/std(b4); %atributo b
+% a1 = a1/std(a1); %atributo a
+% b1 = b1/std(b1); %atributo b
+% 
+% %--- Classe 2 -----
+% a2 = a2/std(a2); %atributo a
+% b2 = b2/std(b2); %atributo b
+% 
+% %--- Classe 3 -----
+% a3 = a3/std(a3); %atributo a
+% b3 = b3/std(b3); %atributo b
+% 
+% %--- Classe 4 -----
+% a4 = a4/std(a4); %atributo a
+% b4 = b4/std(b4); %atributo b
 
 %plota os dados
 plot(a1,b1,'*r') %classe 1 em vermelho
+xlabel('Atributo a'); %eixo a
+ylabel('Atributo b'); %eixo b
 hold on
 plot(a2,b2,'.g') %classe 2 em verde
 plot(a3,b3,'ob') %classe 3 em azul
 plot(a4,b4,'xk') %classe 4 em preto
 
+
 %exibe a tabela de correlação
 corrcoef([a1 a2 a3 b1 b2 b3]);
 cov([a1 a2 a3 b1 b2 b3]);
 
-%Gerando os testes só pra teste mesmo
-%Naive Bayes
-% sm1 = [mean(a1) mean(b1)];
-% sm2 = [mean(a2) mean(b2)];
-% sm3 = [mean(a3) mean(b3)];
-% sm4 = [mean(a4) mean(b4)];
-
-%--- Classe 1 -----
-% teste1 = [randn(10,1) randn(10,1)]; %atributo a e atributo b
-% 
-% %--- Classe 2 -----
-% teste2 = [randn(10,1) + 5 randn(10,1)]; %atributo a e atributo b
-% 
-% %--- Classe 3 -----
-% teste3 = [randn(10,1) randn(10,1) + 5]; %atributo a e atributo b
-% 
-% %--- Classe 3 -----
-% teste4 = [randn(10,1) + 5 randn(10,1) + 5]; %atributo a e atributo b
-% 
-% %testanto com amostras criadas
-% acertos1 = 0;
-% acertos2 = 0;
-% acertos3 = 0;
-% acertos4 = 0;
-% for i=1:10
-%     if norm(teste1(i,:) - sm1) < norm(teste1(i,:) - sm2) && norm(teste1(i,:) - sm1) < norm(teste1(i,:) - sm3) && norm(teste1(i,:) - sm1) < norm(teste1(i,:) - sm4)
-%         acertos1 = acertos1 + 1;
-%     end
-%     if norm(teste2(i,:) - sm2) < norm(teste2(i,:) - sm1) && norm(teste2(i,:) - sm2) < norm(teste2(i,:) - sm3) && norm(teste2(i,:) - sm2) < norm(teste2(i,:) - sm4)
-%         acertos2 = acertos2 + 1;
-%     end
-%     if norm(teste3(i,:) - sm3) < norm(teste3(i,:) - sm1) && norm(teste3(i,:) - sm3) < norm(teste3(i,:) - sm2) && norm(teste3(i,:) - sm3) < norm(teste3(i,:) - sm4)
-%         acertos3 = acertos3 + 1;
-%     end
-%     if norm(teste4(i,:) - sm4) < norm(teste4(i,:) - sm1) && norm(teste4(i,:) - sm4) < norm(teste4(i,:) - sm2) && norm(teste4(i,:) - sm4) < norm(teste4(i,:) - sm3)
-%         acertos4 = acertos4 + 1;
-%     end
-% end
-
+%--- Acertos ---
 acertos1Total = 0;
 acertos2Total = 0;
 acertos3Total = 0;
 acertos4Total = 0;
+
 %---- 10-Fold ----
 for f=1:10
     %amostras de teste
@@ -125,22 +91,22 @@ for f=1:10
     sm3 = [mean(a3) mean(b3)];
     sm4 = [mean(a4) mean(b4)];
     
-    for i=1:length(teste1)
+    for i=1:length(teste1) %Para cada teste
         if norm(teste1(i,:) - sm1) < norm(teste1(i,:) - sm2) && norm(teste1(i,:) - sm1) < norm(teste1(i,:) - sm3) && norm(teste1(i,:) - sm1) < norm(teste1(i,:) - sm4)
-            acertos1 = acertos1 + 1;
+            acertos1 = acertos1 + 1; %acerto da classe 1
         end
         if norm(teste2(i,:) - sm2) < norm(teste2(i,:) - sm1) && norm(teste2(i,:) - sm2) < norm(teste2(i,:) - sm3) && norm(teste2(i,:) - sm2) < norm(teste2(i,:) - sm4)
-            acertos2 = acertos2 + 1;
+            acertos2 = acertos2 + 1; %acerto da classe 2
         end
         if norm(teste3(i,:) - sm3) < norm(teste3(i,:) - sm1) && norm(teste3(i,:) - sm3) < norm(teste3(i,:) - sm2) && norm(teste3(i,:) - sm3) < norm(teste3(i,:) - sm4)
-            acertos3 = acertos3 + 1;
+            acertos3 = acertos3 + 1; %acerto da classe 3
         end
         if norm(teste4(i,:) - sm4) < norm(teste4(i,:) - sm1) && norm(teste4(i,:) - sm4) < norm(teste4(i,:) - sm2) && norm(teste4(i,:) - sm4) < norm(teste4(i,:) - sm3)
-            acertos4 = acertos4 + 1;
+            acertos4 = acertos4 + 1; %acerto da classe 4
         end
     end
-    acertos1Total = acertos1Total + acertos1;
-    acertos2Total = acertos2Total + acertos2;
-    acertos3Total = acertos3Total + acertos3;
-    acertos4Total = acertos4Total + acertos4;
+    acertos1Total = acertos1Total + acertos1; %acerto classe 1 total
+    acertos2Total = acertos2Total + acertos2; %acerto classe 2 total
+    acertos3Total = acertos3Total + acertos3; %acerto classe 3 total
+    acertos4Total = acertos4Total + acertos4; %acerto classe 4 total
 end
